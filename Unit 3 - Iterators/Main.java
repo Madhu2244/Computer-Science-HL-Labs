f0622faae4bb944da70ec7da6d15369bae5629d2
Madhu2244 <63474175+Madhu2244@users.noreply.github.com>
2020-09-30 17:42:39 -0500

    Update Main.java

--- Unit 3 - Iterators/Main.java
+++ Unit 3 - Iterators/Main.java
@@ -7,26 +7,20 @@ public class Main
 {
   public static void main(String[] args) 
   {
-    //Creating my first List
     IteratorTest one = new IteratorTest("a b c a b c a a", "a");
     one.remove();
     System.out.println(one.toString());
+
+    one.setTest("a b c d e f g h i j x x x x x", "x");
+    one.remove();
+    System.out.println(one.toString());
     
-    //Creating my second List
-    IteratorTest two = new IteratorTest("a b c d e f g h i j x x x x x", "x");
-    two.remove();
-    System.out.println(two.toString());
+    one.setTest("1 2 3 4 5 6 a b c a b c b", "b");
+    one.remove();
+    System.out.println(one.toString());
     
-    //Creating my third List   
-    IteratorTest three = new IteratorTest("1 2 3 4 5 6 a b c a b c b", "b");
-    three.remove();
-    System.out.println(three.toString());
-    
-    
-    
-    //Output:
-    [b, c, b, c]
-    [a, b, c, d, e, f, g, h, i, j]
-    [1, 2, 3, 4, 5, 6, a, c, a, c]
+    one.setTest("c i test cc c cccc more c c more chars c", "c");
+    one.remove();
+    System.out.println(one.toString());
   }
 }
