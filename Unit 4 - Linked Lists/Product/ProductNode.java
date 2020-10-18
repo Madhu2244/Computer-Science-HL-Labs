2f443bfd66002f9274fad649167ba90dc9e64852
Madhu2244 <63474175+Madhu2244@users.noreply.github.com>
2020-10-18 17:50:53 -0500

    Create ProductNode.java

---
+++ Unit 4 - Linked Lists/Product/ProductNode.java
@@ -0,0 +1,22 @@
+//author
+public class ProductNode
+{
+  //INSTANCE FIELDS
+  private Product data;
+  private ProductNode next;
+  
+  //CONSTRUCTOR: Make a default and a two parameter overloaded CONSTRUCTOR
+  
+  
+  
+  //Accessors - one for each instance field above
+  
+  
+  //Mutators -- one for each instance field above. 
+  
+  
+  
+  
+  
+  
+}
