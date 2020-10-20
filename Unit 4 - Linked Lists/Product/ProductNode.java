febf5c9bd1ca19016d224e41f4682593eba327b4
Madhu2244 <63474175+Madhu2244@users.noreply.github.com>
2020-10-20 15:01:39 -0500

    Update ProductNode.java

--- Unit 4 - Linked Lists/Product/ProductNode.java
+++ Unit 4 - Linked Lists/Product/ProductNode.java
@@ -1,4 +1,7 @@
-//author
+//author Madhu Sharma
+/**
+ * ProducNode class creates a Linked List of Products
+ */
 public class ProductNode
 {
   //INSTANCE FIELDS
@@ -6,17 +9,50 @@ public class ProductNode
   private ProductNode next;
   
   //CONSTRUCTOR: Make a default and a two parameter overloaded CONSTRUCTOR
+  /**
+   * Constructs a linked list with a node containing a default product and no pointer
+   */
+  public ProductNode()
+  {
+    this.data = new Product();
+    this.next = null;
+  }
+
+  public ProductNode(Product data)
+  {
+    this.data = data;
+    this.next = null;
+  }
   
+  public ProductNode(Product data, ProductNode next)
+  {
+    this.data = data;
+    this.next = next;
+  }
   
   
   //Accessors - one for each instance field above
+  public Product getData()
+  {
+    return data;
+  }
   
+  public ProductNode getNext()
+  {
+    return next;
+  }
+
   
   //Mutators -- one for each instance field above. 
-  
-  
-  
-  
+  public void setData(Product Data)
+  {
+    data = Data;
+  }
+  public void setNext(ProductNode Next)
+  {
+    next = Next;
+  }
+
   
   
 }
